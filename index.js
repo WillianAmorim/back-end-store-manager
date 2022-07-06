141f4de7e58d1ec46dd735ef80711eb10bfde47f
Willian <willian.amorim1@outlook.com>
2022-07-06 19:10:58 -0300

    Iniciando o projeto

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 require('dotenv').config();
 
+// INICIANDO O PROJETO
+
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
