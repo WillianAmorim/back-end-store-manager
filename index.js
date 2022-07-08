6c05dd13b7ed1f2361144f742861dbe18cc7bc32
Willian <willian.amorim1@outlook.com>
2022-07-07 21:32:18 -0300

    criado rota products/:id

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const productsController = require('./controllers/productsController');
 
 app.get('/products', productsController.getAll);
 
+app.get('/products/:id', productsController.getById);
+
 // INICIANDO O PROJETO
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
