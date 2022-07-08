b325ad3efcc36c645abc48a0dc65e0a31a4c65bc
Willian <willian.amorim1@outlook.com>
2022-07-08 00:14:32 -0300

    feito a resposta do caso de falha no catch

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,9 +2,9 @@ const productsService = require('../services/productService');
 const productsModel = require('../models/productsModel');
 
 const getAll = async (_req, res) => {
-    const products = await productsModel.getAll();
+  const products = await productsModel.getAll();
 
-    return res.status(200).json(products);
+  return res.status(200).json(products);
 };
 
 const getById = async (req, res) => {
@@ -22,9 +22,13 @@ const getById = async (req, res) => {
 const create = async (req, res) => {
   const { name } = req.body;
   
+  try {
     const product = await productsService.create(name);
 
     return res.status(201).json(product);
+  } catch (e) {
+    res.status(e.status).json(e.message);
+  }
 };
 
 module.exports = {
