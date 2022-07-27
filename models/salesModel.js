c6fd8dc6f4325f4e3dfc5da9f6e957e293fca760
Willian <willian.amorim1@outlook.com>
2022-07-26 23:43:35 -0300

    feito função que acessa os dados no bd

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,9 +1,15 @@
 const mysql = require('./connection');
 
 const getAllSales = async () => {
-  const sql = 'SELECT * FROM StoreManager.sales;';
+  const sql = `SELECT sp.sale_id AS saleId, s.date, sp.product_Id AS productId, sp.quantity
+    FROM StoreManager.sales AS s
+    INNER JOIN StoreManager.sales_products AS sp
+    ON s.id = sp.sale_id
+    ORDER BY s.id, sp.product_id`;
 
   const [sales] = await mysql.execute(sql);
+
+  console.log(sales);
 
   return sales;
 };
