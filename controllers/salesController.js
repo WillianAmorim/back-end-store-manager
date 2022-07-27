95b6e186ffee54d9893c7c00de7f318bfe0b489f
Willian <willian.amorim1@outlook.com>
2022-07-26 23:44:09 -0300

    feito função que recebe dados do service

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,11 +1,11 @@
 const salesService = require('../services/salesService');
 
-const allSales = async (_req, res) => {
+const getAllSales = async (_req, res) => {
   const sales = await salesService.getAllSales();
 
   return res.status(200).json(sales);
 };
 
 module.exports = {
-  allSales,
+  getAllSales,
 };
