445a55fcba7d3c15d29fab1b75589d6816184687
Willian <willian.amorim1@outlook.com>
2022-08-03 04:23:19 -0300

    retirado rotas do index

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 const app = require('./app');
 require('dotenv').config();
 
-const productsController = require('./controllers/productsController');
-const salesController = require('./controllers/salesController');
-
-app.get('/products/search', productsController.getByQuery);
-
-app.get('/products', productsController.getAllProducts);
-
-app.get('/products/:id', productsController.getById);
-
-app.post('/products', productsController.create);
-
-app.post('/sales');
-
-app.get('/sales', salesController.getAllSales);
-
-app.get('/sales/:id', salesController.getById);
-
-app.put('/products/:id', productsController.update);
-
-app.delete('/products/:id', productsController.remove);
-
-app.delete('/sales/:id', salesController.remove);
-
-app.put('/sales/id');
-
 // INICIANDO O PROJETO
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
