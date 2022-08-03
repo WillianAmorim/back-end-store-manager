4389d19691ad3e4fce74c0a9be39690221e18a3d
Willian <willian.amorim1@outlook.com>
2022-08-03 04:28:39 -0300

    refatorando codigo do salesService

--- services/salesService.js
+++ services/salesService.js
@@ -1,30 +1,26 @@
 const salesModel = require('../models/salesModel');
-const errorMensage = require('../error/notFoundError');
+const { NotFoundError } = require('../middlewares/error');
 
-const getAllSales = async () => {
-  const sales = await salesModel.getAllSales();
+const salesService = {
+  async getAll() {
+    const items = await salesModel.getAll();
+    return items;
+  },
 
-  return sales;
+  async verifyItem(id) {
+    const exists = await salesModel.exists(id);
+    if (!exists) throw new NotFoundError('Sale not found');
+  },
+
+  async getById(id) {
+    const item = await salesModel.getById(id);
+    return item;
+  },
+
+  async remove(id) {
+    await salesModel.remove(id);
+  },
+
 };
 
-const getById = async (id) => {
-  const sale = await salesModel.getById(id);
-
-  if (sale.length <= 0) throw errorMensage(404, { message: 'Sale not found' });
-
-  return sale;
-};
-
-const remove = async (id) => {
-  const sale = await salesModel.getById(id);
-
-  if (sale.length <= 0) throw errorMensage(404, { message: 'Sale not found' });
-
-  await salesModel.remove(id);
-};
-
-module.exports = {
-  getAllSales,
-  getById,
-  remove,
-};
+module.exports = salesService;
