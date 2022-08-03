a2d03e6fbecf12b15774fa8f9b3e1b0370444363
Willian <willian.amorim1@outlook.com>
2022-08-03 04:27:31 -0300

    refatorando codigo do salesModel

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,36 +1,36 @@
 const mysql = require('./connection');
 
-const getAllSales = async () => {
-  const sql = `SELECT sp.sale_id AS saleId, s.date, sp.product_Id AS productId, sp.quantity
-    FROM StoreManager.sales AS s
-    INNER JOIN StoreManager.sales_products AS sp
-    ON s.id = sp.sale_id
-    ORDER BY s.id, sp.product_id`;
+const salesModel = {
+  async getAll() {
+    const sql = `SELECT ps.sale_id AS saleId, sp.date, ps.product_id AS productId, ps.quantity
+      FROM StoreManager.sales AS sp
+      INNER JOIN StoreManager.sales_products AS ps ON sp.id = ps.sale_id
+      ORDER BY sp.id, ps.product_id`;
+    const [items] = await mysql.execute(sql);
+    return items;
+  },
 
-  const [sales] = await mysql.execute(sql);
+  async getById(id) {
+    const sql = `SELECT s.date, sp.product_id AS productId, sp.quantity
+    FROM  StoreManager.sales_products AS sp
+    INNER JOIN StoreManager.sales AS s ON sp.sale_id = s.id
+    WHERE s.id = ?
+    `;
+    const [item] = await mysql.execute(sql, [id]);
+    return item;
+  },
 
-  return sales;
+  async remove(id) {
+    const sql = 'DELETE FROM StoreManager.sales WHERE id = ?';
+    await mysql.execute(sql, [id]);
+  },
+
+  async exists(id) {
+    const sql = 'SELECT 1 FROM StoreManager.sales WHERE id = ?';
+    const [[item]] = await mysql.execute(sql, [id]);
+    return !!item;
+  },
+
 };
 
-const getById = async (id) => {
-  const sql = `SELECT s.date, sp.product_Id AS productId, sp.quantity
-    FROM StoreManager.sales_products AS sp
-    INNER JOIN StoreManager.sales AS s
-    ON sp.sale_id = s.id
-    WHERE s.id = ?;`;
-
-  const [sale] = await mysql.execute(sql, [id]);
-
-  return sale;
-};
-
-const remove = async (id) => {
-  const sql = 'DELETE FROM StoreManager.sales WHERE id = ?';
-  await mysql.execute(sql, [id]);
-};
-
-module.exports = {
-  getAllSales,
-  getById,
-  remove,
-};
+module.exports = salesModel; 
